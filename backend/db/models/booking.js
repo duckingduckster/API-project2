ab487aa7b7d0e8b771b3fe4de419f9364723c5c9
duc <duc_n44@yahoo.com>
2023-07-12 20:42:05 -0600

    add booking migration and model

---
+++ backend/db/models/booking.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init(
+    {
+    // id: {
+    //   allowNull: false,
+    //   autoIncrement: true,
+    //   primaryKey: true,
+    //   type: Sequelize.INTEGER
+    // },
+    spotId: {
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    startDate: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      allowNull: false
+    },
+    endDate: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      allowNull: false
+    },
+    // createdAt: {
+    //   allowNull: false,
+    //   type: DataTypes.DATE,
+    //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    // },
+    // updatedAt: {
+    //   allowNull: false,
+    //   type: DataTypes.DATE,
+    //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    {
+    sequelize,
+    modelName: 'Booking'
+    }
+  );
+  return Booking;
+};
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -12,12 +12,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Spot.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
+    // id: {
+    //   allowNull: false,
+    //   autoIncrement: true,
+    //   primaryKey: true,
+    //   type: DataTypes.INTEGER
+    // },
     ownerId: {
       type: DataTypes.INTEGER
     },
@@ -63,7 +63,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    DataTypes,
     modelName: 'Spot',
   });
   return Spot;
