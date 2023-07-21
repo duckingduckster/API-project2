f369ba17091bb0882fa900b6ebe5f20aac7e5bfa
duc <duc_n44@yahoo.com>
2023-07-21 03:15:02 -0600

    rest of review routes

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -28,7 +28,7 @@ router.get('/current', requireAuth, async(req, res, next)=>{
             {
                 model: Spot,
                 attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name',
-                'price', 'previewImage']
+                'price']
             },
             {
                 model: User,
@@ -70,10 +70,45 @@ router.post('/:reviewId/images', requireAuth, async(req, res, next)=>{
                 url: newReviewImage.url
             })
         }else return res.status(403).json({message:"Maximum number of images for this resource was reached"})
-    }else return res.status(403).json({message: 'Unathorized'})
+
+    }else return res.status(403).json({message:'Unathorized'})
+
 }else return res.status(404).json({message:"Review couldn't be found"})
 })
 
+//edit a review
+router.put('/:reviewId', requireAuth, validateReview, async(req, res, next)=>{
+    const reviewId = req.params.reviewId
+    const userId = req.user.id
+    const { review, stars } = req.body
 
+    const checkReview = await Review.findByPk(reviewId)
+
+    if(checkReview){
+        if(userId === checkReview.userId){
+            await checkReview.update({ review, stars})
+            return res.status(200).json(checkReview)
+
+        }else return res.status(403).json({message:'Unauthorized'})
+
+    }else return res.status(404).json({message:"Review couldn't be found"})
+})
+
+//delete a review
+router.delete('/:reviewId', requireAuth, async(req, res, next)=>{
+    const reviewId = req.params.reviewId
+    const userId = req.user.id
+
+    const checkReview = await Review.findByPk(reviewId)
+
+    if(checkReview){
+        if(userId === checkReview.userId){
+            await checkReview.destroy()
+            return res.status(200).json({message:"Successfully deleted"})
+
+        }else return res.status(403).json({message:'Unauthorized'})
+
+    }else return res.status(404).json({message:"Review couldn't be found"})
+})
 
 module.exports = router;
