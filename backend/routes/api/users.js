8b5ed7032f9d7cd427d87e5ea94545889d408f89
duc <duc_n44@yahoo.com>
2023-07-22 13:20:12 -0600

    fixed signup errors

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 // backend/routes/api/users.js
 const express = require('express');
 const bcrypt = require('bcryptjs');
-
+const { Spot, Booking, User, Review, ReviewImage, SpotImage } = require('../../db/models')
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
@@ -13,16 +13,16 @@ const router = express.Router();
 const validateSignup = [
   check('email')
       .exists({ checkFalsy: true })
-      .withMessage('Please provide a valid email.'),
+      .withMessage("Invalid email"),
   check('username')
       .exists({ checkFalsy: true })
-      .withMessage('Please provide a username with at least 4 characters.'),
+      .withMessage("Username is required"),
   check('firstName')
       .exists({ checkFalsy: true })
-      .withMessage('Please provide a firstName'),
+      .withMessage("First Name is required"),
   check('lastName')
       .exists({ checkFalsy: true })
-      .withMessage('Please provide a lastName'),
+      .withMessage("Last Name is required"),
   check('password')
       .exists({ checkFalsy: true })
       .withMessage('Password must be 6 characters or more.'),
@@ -36,6 +36,28 @@ const validateSignup = [
       const { email, firstName, lastName, password, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({ email, username, firstName, lastName, hashedPassword });
+
+      const emailCheck = await User.findOne({ where : { email: email }})
+
+      const userNameCheck = await User.findOne({ where: { username: username}})
+
+      if(emailCheck){
+        const err = new Error("User already exists")
+        return res.json({
+          message: err.message,
+          statusCode: 500,
+          errors: {email: "User with that email already exists"}
+        })
+      }
+
+      if(userNameCheck){
+        const err = new Error("User already exists")
+        return res.json({
+          message: err.message,
+          statusCode: 500,
+          errors: {username: "User with that username already exists"}
+        })
+      }
 
       const safeUser = {
         id: user.id,
