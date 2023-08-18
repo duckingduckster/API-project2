0ce06c5c0637af907e299e301bc343fe1ad8c1c4
duc <duc_n44@yahoo.com>
2023-08-17 22:50:06 -0600

    phase 1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,71 @@
+// frontend/src/store/session.js
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  const userInfo = {
+    user: {
+      createdAt: data.user.createdAt,
+      email: data.user.email,
+      id: data.user.id,
+      updatedAt: data.user.updatedAt,
+      username: data.user.username,
+      firstName: data.user.firstName,
+      lastName: data.user.lastName
+    }
+  }
+  dispatch(setUser(userInfo.user));
+  return response;
+};
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
