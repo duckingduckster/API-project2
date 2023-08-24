dfff78c572fe4f1eb31c24e848a8d7f4c70f5e35
duc <duc_n44@yahoo.com>
2023-08-23 22:14:08 -0600

    half of signup feature

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -107,7 +107,7 @@ function SignupFormModal() {
         {errors.confirmPassword && (
           <p>{errors.confirmPassword}</p>
         )}
-        <button type="submit">Sign Up</button>
+        <button type="submit" disabled= { email.length < 1 || username.length < 4 || firstName.length < 1 || lastName.length < 1 || password.length < 6 || confirmPassword.length < 1}>Sign Up</button>
       </form>
     </>
   );
