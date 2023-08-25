1935fe8993668b750c7e86f64cc8f9860834c260
duc <duc_n44@yahoo.com>
2023-08-25 09:21:37 -0600

    some css for landing page

--- frontend/src/components/Spots/LandingPage/index.js
+++ frontend/src/components/Spots/LandingPage/index.js
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getSpots } from "../../../store/spots"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import './LandingPage.css'
 
 function SpotList () {
     const dispatch = useDispatch()
@@ -18,26 +19,26 @@ function SpotList () {
     }
 
     return (
-        <div>
+        <div className="landing-page">
         {spots.map((spot)=>{
             const avgRating = spot.avgRating !== undefined && spot.avgRating !== null ? Number(spot.avgRating).toFixed(1) : 'New'
             return (
-                <div className='spotCard tooltip' key={spot.id} onClick={() => spotClick(spot.id)}>
-              <div className='tooltip-spot' title={spot.name}></div>
-              <div className='spot'>
-                <div className='previewImg'>
-                  <img className='spot-img' src={spot.previewImage || null} alt={`${spot.name}`} />
-                </div>
-                <div className='spot-container'>
-                  <p className='location'>
-                    {`${spot.city}, ${spot.state}`}<span className='stars'>★{avgRating || 'New'}</span>
-                  </p>
-                  <p className='spot-name' title={spot.name}>
+            <div className='spot-tile' key={spot.id} onClick={() => spotClick(spot.id)}>
                     <span className='tooltip-text'>{spot.name}</span>
-                  </p>
-                  <p className='price'>
-                    <span className='spot-price'>${spot.price}</span> night
-                  </p>
+                <div className='each-spot'>
+                    <div className='previewImg'>
+                        <img className='spot-img' src={spot.previewImage || null} alt={`${spot.name}`} />
+                    </div>
+                <div className='spot-container'>
+                    <p className='location'>
+                        {`${spot.city}, ${spot.state}`}<span className='stars'>★{avgRating || 'New'}</span>
+                    </p>
+                    <p className='spot-name' title={spot.name}>
+                        <span className='tooltip-text'>{spot.name}</span>
+                    </p>
+                    <p className='price'>
+                        <span className='spot-price'>${spot.price}</span> night
+                    </p>
                 </div>
               </div>
             </div>
